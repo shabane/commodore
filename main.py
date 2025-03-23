6d633a3518ec6645ee6d771836f6941a91867a1e
shabane <m.mohamadshabane@gmail.com>
2025-03-23 05:46:16 +0330

    refactor: rewrite the code: now i just get the user message and send it back to it with distiq with business message and simple user/bot message

--- main.py
+++ main.py
@@ -1,45 +1,21 @@
-#!/usr/bin/env python3
-from telebot_router import TeleBot
-import os
-import yaml
-from telsdk import TelegramClient
-
-app = TeleBot(__name__)
-telegram_client = TelegramClient(os.environ.get('API_KEY'))
-
-prompts = None
-with open(f'{os.environ.get("PROMPTS_FILE", "./prompts.yaml")}', 'r') as fle:
-    prompts = yaml.safe_load(fle)
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 
-@app.route('/ ?(.*)')
-def menu(message, cmd):
-    chat_dest = message['chat']['id']
-    for prompt in prompts.get('commands'):
-        if prompt.get('key') == cmd:
-            app.send_message(chat_dest, f'{prompt.get("message")}') if prompt.get("message") else ...
-
-            if prompt.get('photos'):
-                for photo in prompt.get('photos'):
-                    telegram_client.sendPhoto(photo, chat_dest)
-
-            if prompt.get('audios'):
-                for audio in prompt.get('audios'):
-                    telegram_client.sendAudio(audio, chat_dest)
-
-            if prompt.get('documents'):
-                for document in prompt.get('documents'):
-                    telegram_client.sendDocument(document, chat_dest)
-
-            if prompt.get('videos'):
-                for video in prompt.get('videos'):
-                    telegram_client.sendVideo(video, chat_dest)
-
-            return
-    app.send_message(chat_dest, f'{os.environ.get("WRONG_CMD_MSG", "wrong message!")}')
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.message:
+        await update.message.reply_text(f"not business, ha? {update.message.text}")
+    elif update.business_message:
+        await update.business_message.reply_text(update.business_message.text)
 
 
-if __name__ == '__main__':
-    print("starting...", flush=True)
-    app.config['api_key'] = os.environ.get('API_KEY')
-    app.poll(debug=True)
+def main() -> None:
+    application = Application.builder().token("7898989405:AAH_WGE1nfJ09DLY1-DyGHaxL3CMud1hln4").build()
+
+    application.add_handler(MessageHandler(filters.TEXT, echo))
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
