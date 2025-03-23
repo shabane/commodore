b60e146e3acc8daa5789be63595eb1ce97391c87
shabane <m.mohamadshabane@gmail.com>
2025-03-24 00:37:56 +0330

    feat: support directive in YAML for those who send a wrong command

--- main.py
+++ main.py
@@ -15,8 +15,11 @@ async def director(update: Update.message) -> str:
     #TODO: check file existance before using it path!
     #TODO: check if yaml file is correct and exist!
     #TODO: use caption for each files that we sending.
+    #TODO: reduce this deplicated functions!
+    is_cmd_match = False
     for prompt in prompts.get('commands'):
         if prompt.get('key') == update.text:
+            is_cmd_match = True
             await update.reply_text(f'{prompt.get("message")}') if prompt.get("message") else ...
 
             if prompt.get('photos'):
@@ -35,8 +38,34 @@ async def director(update: Update.message) -> str:
                 for video in prompt.get('videos'):
                     await update.reply_video(Path(video))
 
+    if not is_cmd_match:
+        if prompt := prompts.get('wrong_command'):
+            if messages := prompt.get('message'):
+                for message in messages:
+                    await update.reply_text(f'{message}') if prompt.get("messages") else ...
+
+            if prompt.get('photos'):
+                for photo in prompt.get('photos'):
+                    await update.reply_photo(Path(photo))
+
+            if prompt.get('audios'):
+                for audio in prompt.get('audios'):
+                    await update.reply_audio(Path(audio))
+
+            if prompt.get('documents'):
+                for document in prompt.get('documents'):
+                    await update.reply_document(Path(document))
+
+            if prompt.get('videos'):
+                for video in prompt.get('videos'):
+                    await update.reply_video(Path(video))
+
+        else:
+            print("no wrong/default command set!", flush=True)
+
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    #TODO: use `on` keyword in YAML, which tell that which of this should take the command
     if update.message:
         await director(update.message)
     elif update.business_message:
@@ -46,7 +75,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 def main() -> None:
-    application = Application.builder().token("7898989405:AAH_WGE1nfJ09DLY1-DyGHaxL3CMud1hln4").build()
+    application = Application.builder().token(os.environ.get("API_KEY")).build()
 
     application.add_handler(MessageHandler(filters.TEXT, echo))
 
