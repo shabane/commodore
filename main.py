bf0ad1dd60d8d73733944dc34e9110a05d121fb8
shabane <m.mohamadshabane@gmail.com>
2025-03-23 07:39:34 +0330

    feat: now we support both user message and business messages!:
    this will enable the bot to work with both at the same time.

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import os
+import yaml
+from pathlib import Path
+
+
+prompts = None
+with open(f'{os.environ.get("PROMPTS_FILE", "./prompts.yaml")}', 'r') as fle:
+    prompts = yaml.safe_load(fle)
+
+
+async def director(update: Update.message) -> str:
+    #TODO: check file existance before using it path!
+    #TODO: check if yaml file is correct and exist!
+    #TODO: use caption for each files that we sending.
+    for prompt in prompts.get('commands'):
+        if prompt.get('key') == update.text:
+            await update.reply_text(f'{prompt.get("message")}') if prompt.get("message") else ...
+
+            if prompt.get('photos'):
+                for photo in prompt.get('photos'):
+                    await update.reply_photo(Path(photo))
+
+            if prompt.get('audios'):
+                for audio in prompt.get('audios'):
+                    await update.reply_audio(Path(audio))
+
+            if prompt.get('documents'):
+                for document in prompt.get('documents'):
+                    await update.reply_document(Path(document))
+
+            if prompt.get('videos'):
+                for video in prompt.get('videos'):
+                    await update.reply_video(Path(video))
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message:
-        await update.message.reply_text(f"not business, ha? {update.message.text}")
+        await director(update.message)
     elif update.business_message:
-        await update.business_message.reply_text(update.business_message.text)
+        await director(update.business_message)
+    else:
+        print("No supported message!", flush=True)
 
 
 def main() -> None:
@@ -18,4 +53,5 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    print("starting...", flush=True)
     main()
